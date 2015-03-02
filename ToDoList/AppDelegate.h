efd5f905d31493372e2276b0bbc031267f2feb49
Michael Goldstein <michael@bitstein.org>
2015-03-02 16:39:30 -0600

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Michael Goldstein on 3/2/15.
+//  Copyright (c) 2015 Michael Goldstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
